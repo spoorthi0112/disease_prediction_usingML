7dd440b7e0aba5bc0f3785b0c467c4096edeb0f5
SPOORTHI K <spoorthikumar39@gmail.com>
2026-05-22 13:35:48 +0530

    Add files via upload

---
+++ django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\skc\downloads\disease-prediction-using-django-and-machine-learning-master (3) (1)\.myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
